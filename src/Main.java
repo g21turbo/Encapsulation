fda551f79211894f1655d13e1d0d36bd0bac028b
g21turbo <dg21stew@gmail.com>
2024-02-05 23:05:22 -0800

    updated player class with constructor, getters and setters

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        Player player = new Player();
+        Player player = new Player("Ninja", 100, "Sword");
+//        player.name = "Ninja";
+//        player.health = 100;
+//        player.weapon = "Sword";
+
+        int damage = 10;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = " + player.healthRemaining());
+
+
+        player.loseHealth(110);
+        System.out.println("Remaining health = " + player.healthRemaining());
+
 
     }
 }
