95ecdf6bff0f30505ba85d9dcaa48c56b701bb3e
g21turbo <dg21stew@gmail.com>
2024-02-06 23:47:05 -0800

    created enhanced player class

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        Player player = new Player("Ninja", 100, "Sword");
+//        Player player = new Player("Ninja", 100, "Sword");
 //        player.name = "Ninja";
 //        player.health = 100;
 //        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//
+//
+//        player.loseHealth(110);
+//        System.out.println("Remaining health = " + player.healthRemaining());
 
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("Remaining health = " + player.healthRemaining());
-
-
-        player.loseHealth(110);
-        System.out.println("Remaining health = " + player.healthRemaining());
+        EnhancedPlayer bob = new EnhancedPlayer("Bob", 400, "Sword");
+        System.out.println("Initial health is = " + bob.healthRemaining());
 
 
     }
